ebda661eaac70ce08b48806136aff7829d601106
WLBF <lbf1353@live.com>
2020-08-20 01:54:08 +0800

    project-1 impl a simple key/value store

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,6 @@
 #![deny(missing_docs)]
+//! A simple key/value store.
 
-use std::collections::HashMap;
+pub use kv::KvStore;
 
-pub struct KvStore {
-    map: HashMap<String, String>,
-}
-
-impl Default for KvStore {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl KvStore {
-    pub fn new() -> Self {
-        KvStore {
-            map: HashMap::new(),
-        }
-    }
-
-    pub fn set(&mut self, key: String, value: String) -> Option<String> {
-        self.map.insert(key, value)
-    }
-
-    pub fn get(&self, key: String) -> Option<String> {
-        self.map.get(&key).map(|v| v.to_owned())
-    }
-
-    pub fn remove(&mut self, key: String) -> Option<String> {
-        self.map.remove(&key)
-    }
-}
+mod kv;
