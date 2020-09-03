6087368a08be01e53208bc0630e52538a779a941
WLBF <lbf1353@live.com>
2020-09-03 11:41:28 +0800

    project-2 impl log compaction

--- src/kv.rs
+++ src/kv.rs
@@ -2,8 +2,10 @@ use crate::error::{KvsError, Result};
 use serde::{Deserialize, Serialize};
 use std::collections::{HashMap, BTreeMap};
 use std::fs::{self, File, OpenOptions};
-use std::io::{Seek, SeekFrom, Write, Read};
+use std::io::{self, Seek, SeekFrom, Write, Read};
 use std::path::{Path, PathBuf};
+
+const COMPACTION_THRESHOLD: u64 = 1024 * 1024;
 
 /// The `KvStore` stores string key/value pairs.
 ///
@@ -13,6 +15,7 @@ pub struct KvStore {
     index: BTreeMap<String, Pos>,
     readers: HashMap<u64, File>,
     writer: File,
+    redundant: u64,
 }
 
 impl KvStore {
@@ -23,6 +26,7 @@ impl KvStore {
 
         let current_term = terms.last().map(|t| *t).unwrap_or(0) + 1;
 
+        let mut redundant = 0;
         let mut readers = HashMap::new();
         let mut index = BTreeMap::new();
 
@@ -30,7 +34,7 @@ impl KvStore {
 
         for term in terms {
             readers.insert(term, new_reader(&path, term)?);
-            load_file(term, &mut readers, &mut index)?;
+            redundant += load_file(term, &mut readers, &mut index)?;
         }
 
         let writer = new_writer(&path, current_term)?;
@@ -41,6 +45,7 @@ impl KvStore {
             index,
             readers,
             writer,
+            redundant,
         })
     }
 
@@ -57,7 +62,12 @@ impl KvStore {
         self.writer.flush()?;
         let new_offset = self.writer.seek(SeekFrom::Current(0))?;
         let pos = Pos { term: self.current_term, offset, len: new_offset - offset };
-        self.index.insert(key, pos);
+        self.index.insert(key, pos).map(|p| self.redundant += p.len);
+
+        if self.redundant > COMPACTION_THRESHOLD {
+            self.compact()?;
+        }
+
         Ok(())
     }
 
@@ -67,7 +77,6 @@ impl KvStore {
     pub fn get(&mut self, key: String) -> Result<Option<String>> {
         if let Some(pos) = self.index.get(&key) {
             let reader = self.readers.get_mut(&pos.term).expect("log reader not found");
-
             reader.seek(SeekFrom::Start(pos.offset))?;
             let cmd_reader = reader.take(pos.len);
 
@@ -88,29 +97,76 @@ impl KvStore {
             let cmd = Command::Remove { key: key.clone() };
             serde_json::to_writer(&self.writer, &cmd)?;
             self.writer.flush()?;
-            self.index.remove(&key);
+            self.index.remove(&key).map(|p| self.redundant += p.len);
             return Ok(());
+        }
+
+        if self.redundant > COMPACTION_THRESHOLD {
+            self.compact()?;
         }
 
         Err(KvsError::KeyNotFound)
     }
+
+    fn compact(&mut self) -> Result<()> {
+        let compact_term = self.current_term + 1;
+        self.current_term += 2;
+
+        println!("compact {}", compact_term);
+
+        let mut compact_writer = new_writer(&self.path, compact_term)?;
+        self.writer = new_writer(&self.path, self.current_term)?;
+
+        let compact_reader = new_reader(&self.path, compact_term)?;
+        let current_reader = new_reader(&self.path, compact_term)?;
+        self.readers.insert(compact_term, compact_reader);
+        self.readers.insert(self.current_term, current_reader);
+
+        let mut offset: u64 = 0;
+        for pos in self.index.values_mut() {
+
+            let reader = self.readers.get_mut(&pos.term).expect("log reader not found");
+            reader.seek(SeekFrom::Start(pos.offset))?;
+            let mut cmd_reader = reader.take(pos.len);
+
+            let len = io::copy(&mut cmd_reader, &mut compact_writer)?;
+
+            *pos = Pos { term: compact_term, offset, len: pos.len };
+            offset += len;
+        }
+        compact_writer.flush()?;
+
+        let stale_terms: Vec<u64> = self.readers.keys().filter(|&&t| t < compact_term).cloned().collect();
+        for term in stale_terms {
+            self.readers.remove(&term);
+            let path = log_path(&self.path, term);
+            fs::remove_file(path)?
+        }
+
+        self.redundant = 0;
+        Ok(())
+    }
 }
 
-fn load_file(term: u64, readers: &mut HashMap<u64, File>, index: &mut BTreeMap<String, Pos>) -> Result<()> {
+fn load_file(term: u64, readers: &mut HashMap<u64, File>, index: &mut BTreeMap<String, Pos>) -> Result<u64> {
     let reader = readers.get_mut(&term).expect("log reader not found");
     let mut stream = serde_json::Deserializer::from_reader(reader).into_iter::<Command>();
     let mut offset: u64 = 0;
+    let mut redundant: u64 = 0;
     while let Some(res) = stream.next() {
         let new_offset = stream.byte_offset() as u64;
         let pos = Pos { term, offset, len: new_offset - offset };
         match res? {
-            Command::Set { key, .. } => index.insert(key, pos),
-            Command::Remove { key } => index.remove(&key),
+            Command::Set { key, .. } => index.insert(key, pos).map(|p| redundant += p.len),
+            Command::Remove { key } => {
+                redundant += new_offset - offset;
+                index.remove(&key).map(|p| redundant += p.len)
+            },
         };
         offset = new_offset;
     }
 
-    Ok(())
+    Ok(redundant)
 }
 
 fn new_reader(dir: &Path, term: u64) -> Result<File> {
@@ -159,6 +215,7 @@ enum Command {
     Remove { key: String },
 }
 
+#[derive(Debug)]
 struct Pos {
     term: u64,
     offset: u64,
